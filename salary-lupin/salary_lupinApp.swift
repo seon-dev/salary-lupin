88a827061452eb65b9fb551c64f5fc3558aedcd1
seoeunbae <enyoupark@gmail.com>
2024-10-10 08:24:22 +0900

    Initial Commit

---
+++ salary-lupin/salary_lupinApp.swift
@@ -0,0 +1,17 @@
+//
+//  salary_lupinApp.swift
+//  salary-lupin
+//
+//  Created by baeseoeun on 10/10/24.
+//
+
+import SwiftUI
+
+@main
+struct salary_lupinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ salary-lupinTests/salary_lupinTests.swift
@@ -0,0 +1,17 @@
+//
+//  salary_lupinTests.swift
+//  salary-lupinTests
+//
+//  Created by baeseoeun on 10/10/24.
+//
+
+import Testing
+@testable import salary_lupin
+
+struct salary_lupinTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
